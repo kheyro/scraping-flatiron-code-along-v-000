b38bb8500abeffb57c2116d1493ba6dc7411aaa3
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-15 00:56:10 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,6 +25,8 @@ class Scraper
   end
 
   def get_courses
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    @doc = Nokogiri::HTML(html)
     @doc.css("h2")
   end
 
